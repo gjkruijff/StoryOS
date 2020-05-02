a2fd04fb2e01d8a886a315682e18e3feda0433c5
gjkruijff <gj.kruijff@gmail.com>
2020-05-02 11:57:29 +0200

    NODEJS U1 main.js zip lookup

---
+++ nodejs/recipe_connection/main.js
@@ -0,0 +1,12 @@
+// Main module for Recipe Connection web app
+
+// @author GJ Kruijff
+// @email  gj.kruijff@gmail.com
+// @date   Sat 2 May 2020
+
+"use strict";
+
+const cities = require("cities");
+
+var myCity = cities.zip_lookup("66440");
+console.log(myCity);
