493abafea433dbc8b7426aa333ab581bbfb5d366
gjkruijff <gj.kruijff@gmail.com>
2020-05-02 12:31:22 +0200

    Adding hostname

--- nodejs/simple_server/main.js
+++ nodejs/simple_server/main.js
@@ -11,12 +11,13 @@ const port = 3000,
   http = require("http"),
   httpStatus = require("http-status-codes"),
   app = http.createServer((request, response) => {
-    console.log("MAIN: Received an incoming request");
+    console.log(`Hostname is ${os.hostname()}`);
+    console.log(`${os.hostname()}: Received an incoming request`);
     response.writeHead(httpStatus.OK, {
       "Content-Type": "text/html"
     });
 
-    let responseMessage = "<h1>Hello, Universe!</h1>";
+    let responseMessage = "<h1>Hello, Universe from "+os.hostname()+"!</h1>";
     response.write(responseMessage);
     response.end();
     console.log(`MAIN: Sent a response: ${responseMessage}`);
